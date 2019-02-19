72de2f1273acf0553e979f6fbdd5061becae684c
Feihu <guishizuiezhishi@163.com>
2019-02-19 17:10:56 +0800

    NindexTest updated

--- NindexTest/public/js/wssIndex.js
+++ NindexTest/public/js/wssIndex.js
@@ -2,26 +2,53 @@
  * wssIndex
  */
 (function ($) {
-    let contentContainer = $(".socket-content");
+    let contentContainer0 = $(".socket-content:eq(0)");
+    let contentContainer1 = $(".socket-content:eq(1)");
+    const MESSAGE = "chatMessage",SERVER="服务器",CLIENT="客户端";
+    const USER_NAME ="LFH";
+    const EMPTY="";
     //创建socket连接
-   let socket = io("http://127.0.0.1:8901/chat");
-   socket.on("connect",function () {
+   const socket0 = io("http://127.0.0.1:8901/chat"),socket1=io("http://127.0.0.1:8901/talk"),
+    socket = [{$$:socket0,name:`${USER_NAME}:socket0`},{$$:socket1,name:`${USER_NAME}:socket1`}];
+   let connectFn=function(){
        console.info("Socket opened.");
-       socket.emit("Hi, Server!");
-   });
-   socket.on("disconnected",function () {
+       this.$$.emit(MESSAGE,{text:this.name});
+       this.$$.emit("login",{username:this.name});
+   };
+    socket0.on("connect",connectFn.bind(socket[0]));
+    socket1.on("connect",connectFn.bind(socket[1]));
+    socket0.on("disconnected",function () {
        console.info("Socket closed.");
-       console.info(arguments);
    });
-   socket.on("news",function (data) {
-       console.info(data);
-       contentContainer.html(JSON.stringify(data));
+    socket0.on("news",function (data) {
+       contentContainer0.append(`<p><p class="pull-left"><b class="chat-label">${SERVER}</b> ${JSON.stringify(data)}</p></p>`);
    });
-
-    $(".socketLink").click(function () {
-        var $input=$(this).parent().find("input.form-control");
-        if($input&&$input.length){
-            socket.send($input.val());
+    socket0.on("chatSend",function (data) {
+        contentContainer0.append(`<p><p class="pull-left"><b  class="chat-label">${SERVER}</b> ${JSON.stringify(data)}</p></p>`);
+    });
+    socket1.on("news",function (data) {
+        contentContainer1.append(`<p><p class="pull-left"><b class="chat-label">${SERVER}</b> ${JSON.stringify(data)}</p></p>`);
+    });
+    socket1.on("chatSend",function (data) {
+        contentContainer1.append(`<p><p class="pull-left"><b  class="chat-label">${SERVER}</b> ${JSON.stringify(data)}</p></p>`);
+    });
+    $(".socketLink:eq(0)").click(function () {
+        var $input=$(this).parent().find("input.form-control"),$inputVal = $input.val();
+        if($inputVal&&$inputVal.length){
+            let data={targetName:socket[1].name,text:$inputVal};
+            contentContainer0.append(`<p><p class="pull-right"><b class="chat-label">${CLIENT}</b>${JSON.stringify(data)}</p></p>`);
+            socket0.emit(MESSAGE,data);
+            $input.val(EMPTY);
         }
     });
+    $(".socketLink:eq(1)").click(function () {
+        var $input=$(this).parent().find("input.form-control"),$inputVal = $input.val();
+        if($inputVal&&$inputVal.length){
+            let data={targetName:socket[0].name,text:$inputVal};
+            contentContainer1.append(`<p><p class="pull-right"><b class="chat-label">${CLIENT}</b>${JSON.stringify(data)}</p></p>`);
+            socket1.emit(MESSAGE,data);
+            $input.val(EMPTY);
+        }
+    });
+
 }(jQuery));
--- NindexTest/wssRoutes/socket.bind.js
+++ NindexTest/wssRoutes/socket.bind.js
@@ -5,15 +5,17 @@
  * @description
  */
 let users=[];
+let socketsEntry = {};
 let socketInit=function($io,$fn){
     $io.on('connection',(socket)=>{
-        console.info('connection');
         socket.emit('news', { hello: 'world' });
 
         // 失去连接
         socket.on('disconnect',function(){
-            if(users.indexOf(socket.username)>-1){
-                users.splice(users.indexOf(socket.username),1);
+            let userName = socket.username;
+            if(users.indexOf(userName)>-1){
+                users.splice(users.indexOf(userName),1);
+                delete socketsEntry[userName];
                 console.info(socket.username+'===>disconnected');
             }
 
@@ -22,7 +24,6 @@ let socketInit=function($io,$fn){
 
         //消息传递
         socket.on('message',function(data){
-            console.info('message');
             let newData = {text: data.text, user: socket.username};
             socket.emit("news",newData);
             socket.emit('receive_message',newData);
@@ -32,11 +33,13 @@ let socketInit=function($io,$fn){
         //登录
         socket.on('login',function(data){
             console.info('login');
-            if(users.indexOf(data.username)>-1){
+            let userName = data.username;
+            if(users.indexOf(userName)>-1){
 
             }else{
-                socket.username = data.username;
-                users.push(data.username);
+                socket.username = userName;
+                users.push(userName);
+                socketsEntry[userName]=socket;
                 // 统计连接数
                 socket.emit('users',{number:users.length});  // 发送给自己
                 socket.broadcast.emit('users',{number:users.length}); // 发送给其他人
@@ -50,6 +53,25 @@ let sockets={
     "/chat":function ($io) {
         socketInit($io,function (socket) {
             console.info(121);
+            socket.on("chatMessage",function (data) {
+                let userName = data.targetName;
+                if(users.indexOf(userName)>-1){
+                    let $socket=socketsEntry[userName];
+                    $socket.emit("chatSend",Object.assign({time:new Date()},data));
+                }
+            });
+        });
+    },
+    "/talk":function ($io) {
+        socketInit($io,function (socket) {
+            console.info(121.121);
+            socket.on("chatMessage",function (data) {
+                let userName = data.targetName;
+                if(users.indexOf(userName)>-1){
+                    let $socket=socketsEntry[userName];
+                    $socket.emit("chatSend",Object.assign({time:new Date()},data));
+                }
+            });
         });
     }
 };
